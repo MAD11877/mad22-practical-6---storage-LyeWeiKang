cee505942e74ae55d6eba6e6dc0addbbca271fe3
Wei Kang Lye <s10221838@connect.np.edu.sg>
2022-05-26 01:32:14 +0800

    Finish Practical 6

--- app/src/main/java/sg/edu/np/mad/mad_practical2/LoginActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad_practical2/LoginActivity.java
@@ -2,6 +2,11 @@ package sg.edu.np.mad.mad_practical2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Users");
+        myRef.setValue("11877");
+        myRef.setValue("mad");
+        Button loginButton=findViewById(R.id.LoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference nested=myRef.child("mad").push();
+                nested.setValue("11877");
+                nested.setValue("mad");
+            }
+        });
+
     }
 }
